6cbf62aa68953a0dc00432c4fa161b7d31bd6c68
Michael Kearney <mkearney@ku.edu>
2017-04-01 20:55:26 -0500

    added renviron doc to readme refine function

--- R/warcraft.r
+++ R/warcraft.r
@@ -1,21 +1,28 @@
-warcraft3 <- function() {
-    dir <- path.expand("~/")
+warcraft3 <- function(download = FALSE) {
     mp3s <- WARCRAFT_PATH()
     if (identical(mp3s, FALSE)) {
         mp3s <- grep("^.*-warcraft.mp3$",
-                     list.files(dir, all.files = TRUE, full.names = TRUE),
+                     list.files(all.files = TRUE, full.names = TRUE),
                      value = TRUE)
-        if (length(mp3s == 0)) {
+        if (length(mp3s == 0) & download) {
             warning("could not find ^.*-warcraft.mp3$ files.",
                     call. = FALSE)
-            message("installing jobsdone audio...")
-            mp3s <- file.path(dir, ".jobsdone.mp3")
-            download.file(
-                "https://dl.dropboxusercontent.com/u/94363099/jobsdone.mp3",
-                destfile = mp3s)
+            message("installing warcraft3 audio files...")
+            download.file("https://dl.dropboxusercontent.com/u/94363099/.icandothat-warcraft.mp3",
+                          ".icandothat-warcraft3.mp3")
+            download.file("https://dl.dropboxusercontent.com/u/94363099/.workwork-warcraft.mp3",
+                          ".workwork-warcraft.mp3")
+            download.file("https://dl.dropboxusercontent.com/u/94363099/.jobsdone-warcraft.mp3",
+                          ".jobsdone-warcraft.mp3")
+            mp3s <- grep("^.*-warcraft.mp3$",
+                         list.files(all.files = TRUE, full.names = TRUE),
+                         value = TRUE)
+            mp3s <- paste(mp3s, collapse = ":")
+            Sys.setenv(WARCRAFT_PATH = mp3s)
+        } else {
+            stop("sorry, you'll need to download the warcraft audio files",
+                 call. = FALSE)
         }
-        mp3s <- paste(mp3s, collapse = ":")
-        Sys.setenv(WARCRAFT_PATH = mp3s)
     }
     if (runif(1) < .10) {
         mp3 <- sample(mp3s, 1)
